f6c2faf3e50c481745ba29304dee0703520832d0
Alena Miloserdova <miloserdovaalena220@gmail.com>
2023-03-07 02:27:13 +0300

    лаб2

---
+++ лаб2/back.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <textarea name="text" id="" cols="70" rows="70">
+            <?php
+                print_t(get_headers("https://www.internet-technologies.ru/articles/php-v-html.html"));
+            ?>
+        </textarea>
+    </main>
+</body>
+</html>
