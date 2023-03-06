3693982a864b995dd1bbb1b3edf72086843ec527
Alena Miloserdova <miloserdovaalena220@gmail.com>
2023-03-07 01:11:56 +0300

    лаб1

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <header class="header">
+        <div class="header__wrapper">
+            <img src="img/logo_mospoly.png" alt="" class="header__logo">
+            <h1 class="header__text">Домашняя работа: Hello, World!</h1>
+        </div>
+      </header>
+
+      <main>
+        <section>
+            <div class="hero__wrapper">
+                <?
+                echo 'ПРИВЕТ МИР!';
+                ?>
+            </div>
+        </section>
+      </main>
+
+      <footer class="footer">
+        <div class="footer__wrapper">
+            <p class="footer__text">Милосердова Алёна Владимировна 221-322</p>
+        </div>
+      </footer>
+</body>
+
+</html>
+</body>
+</html>
